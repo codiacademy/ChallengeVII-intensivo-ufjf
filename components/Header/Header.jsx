90c6ee234cc98f606fa4eba762f7d81e56e5bd91
LeonelPrestes <leonelprestes90@gmail.com>
2025-07-16 21:23:37 -0300

    botão para abrir sidebar

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,12 +1,17 @@
-import UserMenu from "./Menu.jsx";
+import UserMenu from "./UserMenu.jsx";
 export default function Header() {
   return (
     <header className="w-full flex bg-[var(--roxo)] py-2 h-12 mt-0 " >
       <div className="left  text-4m text-[#FFF] flex justify-between w-1/2 ml-8">
+        <div className=" rounded-full w-2 md:hidden flex items-center justify-end p-4">
 
+
+            <UserMenu />
+   
+        </div>
       </div>
       <div className="right justify-items-end w-1/2">
-        <UserMenu/>
+        <UserMenu />
       </div>
     </header>
   );
