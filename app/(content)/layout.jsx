25c89826d1c870ca04bede898c2c672b46144467
LeonelPrestes <leonelprestes90@gmail.com>
2025-07-24 18:02:48 -0300

    finalizado

--- app/(content)/layout.jsx
+++ app/(content)/layout.jsx
@@ -14,13 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="PT-BR" className={inter.className}>
-      <body className="h-screen flex flex-col overflow-hidden">
+      <body className="h-screen flex flex-col overflow-hidden ">
         <Header />
-        <div className="flex flex-1 overflow-hidden bg-[var(--colors-background)]">
+        <div className="flex flex-1 overflow-hidden bg-[var(--colors-background)] ">
           <div className="hidden md:flex flex-col w-[250px] bg-[var(--colors-sidebar)]">
             <SideBar />
           </div>
-          <div className="w-full overflow-auto">
+          <div className="w-full overflow-auto ">
             <main className="min-h-11/12 m-0 md:m-7 p-0 md:p-3 flex-1 flex-grow rounded-[15px] shadow-[10px_10px_20px_#a3b1c6,-10px_-10px_20px_#ffffff] bg-[var(--colors-background)]">{children}
             </main>
           </div>
